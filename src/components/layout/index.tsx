18e1f085556d44fcbab979df496ed499a4df64b5
vidga471@gmail.com <vidga471@gmail.com>
2024-05-14 08:59:36 +0300

    add nav elements

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Container } from '../container';
+import { NavBar } from '../nav-bar';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Header } from '../header';
 
 export const Layout = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <div className="flex-2 p-4">
+          <NavBar />
+        </div>
+        <div className="flex-1 p-4">
+          <Outlet />
+        </div>
+        <div className="flex-2 p-4"></div>
+      </Container>
+    </>
+  );
 };
