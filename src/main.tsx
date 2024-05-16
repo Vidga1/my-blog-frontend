8458b2fdc975b812084a3721fc97e1fd311926b7
vidga471@gmail.com <vidga471@gmail.com>
2024-05-16 03:29:45 +0300

    add components in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,59 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './App';
-import { store } from './app/store';
-import './index.css';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { NextUIProvider } from '@nextui-org/react';
-import { createBrowserRouter } from 'react-router-dom';
+import { store } from './app/store';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import { Auth } from './pages/auth';
+import { Posts } from './pages/posts';
 import { ThemeProvider } from './components/theme-provider';
-
-const container = document.getElementById('root');
+import { Layout } from './components/layout';
+import { UserProfile } from './pages/user-profile';
+import { CurrentPost } from './pages/current-post';
+import { Followers } from './pages/followers';
+import { Following } from './pages/following';
 
 const router = createBrowserRouter([
   {
     path: '/auth',
-    element: <h1>Auth</h1>,
+    element: <Auth />,
   },
   {
     path: '/',
-    element: <h1>Layout</h1>,
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Posts />,
+      },
+      {
+        path: 'posts/:id',
+        element: <CurrentPost />,
+      },
+      {
+        path: 'users/:id',
+        element: <UserProfile />,
+      },
+      {
+        path: 'followers',
+        element: <Followers />,
+      },
+      {
+        path: 'following',
+        element: <Following />,
+      },
+    ],
   },
 ]);
 
-if (container) {
-  const root = createRoot(container);
-
-  root.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <NextUIProvider>
-          <ThemeProvider>
-            <App />
-          </ThemeProvider>
-        </NextUIProvider>
-      </Provider>
-    </React.StrictMode>,
-  );
-} else {
-  throw new Error(
-    "Root element with ID 'root' was not found in the document. Ensure there is a corresponding HTML element with the ID 'root' in your HTML file.",
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>,
+);
