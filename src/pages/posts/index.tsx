523f065cf276dc6be448016da50cc67e588cdd9b
vidga471@gmail.com <vidga471@gmail.com>
2024-05-17 00:14:35 +0300

    add card with posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { Card } from '../../components/card';
 import { CreatePost } from '../../components/create-post';
 import { useGetAllPostsQuery } from '../../app/services/postsApi';
 
@@ -10,6 +10,34 @@ export const Posts = () => {
       <div className="mb-10 w-full flex">
         <CreatePost />
       </div>
+      {data && data.length > 0
+        ? data.map(
+            ({
+              content,
+              author,
+              id,
+              authorId,
+              comments,
+              likes,
+              likedByUser,
+              createdAt,
+            }) => (
+              <Card
+                key={id}
+                avatarUrl={author.avatarUrl ?? ''}
+                content={content}
+                name={author.name ?? ''}
+                likesCount={likes.length}
+                commentsCount={comments.length}
+                authorId={authorId}
+                id={id}
+                likedByUser={likedByUser}
+                createdAt={createdAt}
+                cardFor="post"
+              />
+            ),
+          )
+        : null}
     </>
   );
 };
